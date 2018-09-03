efaddfd828accae595a5e8c2e963fd1813add884
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-03 22:47:42 +0200

    Updated Heap code.

--- interpreter/heap.cs
+++ interpreter/heap.cs
@@ -1,10 +1,15 @@
 //Heap-related routines and types
 namespace Mirix.Interpreter.Data.Heap
 {
+
+    using System.Collections.Generic;
+
     //Heap structure
     public sealed class Heap
     {
         //Add stuff here
+        //Heap items
+        private List<HeapItem> heapItems = new List<HeapItem>();
 
         public Heap()
         {
@@ -14,7 +19,19 @@ namespace Mirix.Interpreter.Data.Heap
         //Clears all data from the Heap
         public void clear()
         {
-
+            heapItems.Clear();
         }
+
+        //Add an item to the heap
+        public void addItem(HeapItem item)
+        {
+            heapItems.Add(item);
+        }
+    }
+
+    //Represents an item on the heap
+    public class HeapItem
+    {
+
     }
 }
