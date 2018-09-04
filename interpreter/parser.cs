ee62cf2eace8c3bcfb3e1d517b05d4d27539a7d5
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-04 22:10:29 +0200

    Added todo item for tomorrow.

--- interpreter/parser.cs
+++ interpreter/parser.cs
@@ -47,6 +47,7 @@ namespace Mirix
                                 //If we get the keyword `function` then we can go ahead
                                 //TODO: Remove me after halt test
                                 Console.Out.WriteLine("HALT");
+                                //DONE I am right increment and impement me actually lol
                                 continue;
                             }
                             else
